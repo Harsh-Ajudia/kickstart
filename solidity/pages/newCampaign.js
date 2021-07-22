8e68ced58ea65a9253fe572ca07a14f9a1cc8e1b
Neosoft <harshajudia99@gmail.com>
2021-07-22 18:19:50 +0530

    added basic nextjs

---
+++ solidity/pages/newCampaign.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function newCampaign() {
+    return (
+        <div>
+            <h1>This is the new Campaign page</h1>
+        </div>
+    )
+}
---
+++ solidity/pages/show.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function show() {
+    return (
+        <div>
+            <h1>Showcase</h1>
+        </div>
+    )
+}
