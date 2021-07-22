d832f0deb4f55888f872961df1c6e4e62d11b88b
Neosoft <harshajudia99@gmail.com>
2021-07-22 15:53:55 +0530

    written few tests for campaign

--- solidity/test/Campaign.test.js
+++ solidity/test/Campaign.test.js
@@ -14,7 +14,7 @@ let campaignAddress
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
-    factory = await web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
         .send({ from: accounts[0], gas: '1000000' })
 
@@ -24,5 +24,23 @@ beforeEach(async () => {
     const addresses = await factory.methods.getDeployedCampaigns().call()
     campaignAddress = addresses[0]
 
-    
+    campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress)
+})
+
+describe('Campaigns', () => {
+    it('deploys a factory and a campaign', () => {
+        assert.ok(factory.options.address)
+        assert.ok(campaign.options.address)
+    })
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call()
+        assert.equal(manager, accounts[0])
+    })
+
+    it('allows people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({ value: '200', from: accounts[1] })
+        const isContributor = await campaign.methods.approvers(accounts[1]).call()
+        assert(isContributor)
+    })
 })
