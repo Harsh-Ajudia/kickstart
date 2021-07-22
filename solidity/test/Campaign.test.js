8af5a3b84f7c0906b774127b2a5be237f8f5ab11
Neosoft <harshajudia99@gmail.com>
2021-07-22 16:59:49 +0530

    kickstart contract completed and deployed

--- solidity/test/Campaign.test.js
+++ solidity/test/Campaign.test.js
@@ -43,4 +43,43 @@ describe('Campaigns', () => {
         const isContributor = await campaign.methods.approvers(accounts[1]).call()
         assert(isContributor)
     })
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({ from: accounts[1], value: '5' })
+            assert(false)
+        } catch (error) {
+            assert(error)
+        }
+    })
+
+    it('allows a manager to make a payment request', async () => {
+        await campaign.methods.createRequest('Buy Batteries', '100', accounts[1])
+            .send({ from: accounts[0], gas: '1000000' })
+
+        const request = await campaign.methods.requests(0).call()
+        // console.log(request)
+        assert.equal('Buy Batteries', request.description)
+    })
+
+    it('processes a request', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        })
+
+        await campaign.methods
+            .createRequest('Buy Motors', web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({ from: accounts[0], gas: '1000000' })
+
+        await campaign.methods.approveRequest(0).send({ from: accounts[0], gas: '1000000' })
+
+        await campaign.methods.finalizeRequest(0).send({ from: accounts[0], gas: '1000000' })
+
+        let balance = await web3.eth.getBalance(accounts[1])
+        balance = web3.utils.fromWei(balance, 'ether')
+        balance = parseFloat(balance)
+        console.log(balance)
+        assert(balance > 103)
+    })
 })
